5abf8b1ff689d2e6a3dcb1d5b3704443454b0753
Krzysztof Sciubisz <krzysztof.sciubisz@gmail.com>
2013-08-30 12:50:56 +0200

    remove not used methods

--- app/services/data.js
+++ app/services/data.js
@@ -3,33 +3,6 @@
 app.factory('Data', function() {
   return {
     prefix: 'pomodoro-',
-  
-    add: function() {
-      obj = {
-        model: 'Task',
-        item: {
-          id: 1,
-          status: dupa
-        }
-      }
-      localStorage.setItem(prefix + obj.model + '-' + id, obj);
-    },
-    
-    remove: function(obj) {
-      localStorage.removeItem(prefix + obj.model + '-' + id);
-    },
-    
-    update: function(obj) {
-      this.add(obj);
-      return true;
-    },
-    
-    find: function(model, id) {
-    },
-    
-    collection: function(model) {
-      
-    },
     
     sync: function(model, collection) {
       localStorage.setItem(prefix + model, JSON.stringify(collection));
